e821dfe9596f5b732eaaaedbdad4e5bf601a88fc
Sethuram Ramalinga Reddy <ramalins@tcd.ie>
2020-06-14 22:37:43 +0100

    compiled angular package

---
+++ dist/SmartShareFrontEnd/assets/js/env.js
@@ -0,0 +1,8 @@
+(function(window) {
+  window["env"] = window["env"] || {};
+
+  // Environment variables
+  window["env"]["gatewayUrl"] = "http://localhost:8081";
+  window["env"]["frontendPort"] = "4200";
+  window["env"]["debug"] = true;
+})(this);
